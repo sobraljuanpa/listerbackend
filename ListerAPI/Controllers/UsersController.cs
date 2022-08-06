c119e9bf4abcfa590f600f8bc770ce1520a73355
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2022-08-06 16:51:29 -0300

    JWT Authentication working

--- ListerAPI/Controllers/UsersController.cs
+++ ListerAPI/Controllers/UsersController.cs
@@ -1,9 +1,17 @@
 ﻿using ListerAPI.Models;
 using ListerAPI.Services;
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ListerAPI.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class UsersController
@@ -12,6 +20,33 @@ namespace ListerAPI.Controllers
         public UsersController(IUserService userService)
         {
             service = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public ActionResult Login([FromBody] User user)
+        {
+            User aux = service.GetUsers().Find(u => user.UserName.Equals(u.UserName));
+            if (aux != null)
+            {
+                var key = Encoding.ASCII.GetBytes("Testing key tiene que ser mas larga");
+
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, aux.UserName)
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(10),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return new AcceptedResult("",tokenHandler.WriteToken(token));
+            }
+
+            return new UnauthorizedResult();
         }
 
         [HttpGet]
--- ListerAPI/Program.cs
+++ ListerAPI/Program.cs
@@ -1,4 +1,7 @@
 using ListerAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +10,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserService, UserService>();
 
+var key = Encoding.ASCII.GetBytes("Testing key tiene que ser mas larga");
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+
 var app = builder.Build();
+
+app.UseCors();
+app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 
